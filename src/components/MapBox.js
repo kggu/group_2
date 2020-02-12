21480f546b2d533d07a2b5e991f8390db55586dc
kggu <miikkakortelainen1@gmail.com>
2020-02-12 14:38:54 +0200

    add test marker to mapbox

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react'
-import MapGL from 'react-map-gl'
+import MapGL, {Marker } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 const TOKEN="pk.eyJ1IjoibWlpa2tha3IiLCJhIjoiY2s2ZjJ6bGw4MGpzazNkcXB5ZXZ3aWc5ZiJ9.yXrQeEmRmdZdskrElLI8Qg"
@@ -16,15 +16,19 @@ const Map = () => {
     zoom: 16
   })
 
-  const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000 })
+  const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 500 })
   
   return (
       <MapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/mapbox/dark-v8"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
         onViewportChange={_onViewportChange}
       >
+        <Marker longitude={25.473} latitude={65.013}>
+        <div style={{color: 'black'}}>TEST</div>
+        </Marker>
+
       </MapGL>
   )
 }
