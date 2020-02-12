3c09ec31afb86d42d8d404ffde1ec5f43d87bef5
Sami Jouppila <sami.jouppila@gmail.com>
2020-02-12 09:40:01 +0200

    New React project with OAuth0 working

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// src/App.js
+
+import React from "react";
+import NavBar from "./components/NavBar";
+
+// New - import the React Router components, and the Profile page component
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+
+// NEW - import the PrivateRoute component
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
