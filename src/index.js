d227b9c81cbcd9ed6d0304e6c91e2ece66317504
Sami Jouppila <sami.jouppila@gmail.com>
2020-02-12 12:53:37 +0200

    Changed from JSON-based auth-config file to .env file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
-import config from "./auth_config.json";
 import history from "./utils/history";
 
 // A function that routes the user to the right place
@@ -20,8 +19,8 @@ const onRedirectCallback = appState => {
 
 ReactDOM.render(
   <Auth0Provider
-    domain={config.domain}
-    client_id={config.clientId}
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    client_id={process.env.REACT_APP_AUTH_CLIENT}
     redirect_uri={window.location.origin}
     onRedirectCallback={onRedirectCallback}
   >
