c5b6a550b0f0fc851766e9076335b39d23394b1c
Sami Jouppila <sami.jouppila@gmail.com>
2020-02-20 12:12:05 +0200

    API get request function working, .env updated for api root value

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,20 @@
 
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
-
-
-// NEW - import the Link component
 import { Link } from "react-router-dom";
 import "./NavBar.css";
+import axios from "axios";
 
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  const sendGetRequest = async (longitude, latitude, range) => {
+    const address = process.env.REACT_APP_API_ROOT + "/hotspot/search?longitude=" + longitude + "&latitude=" + latitude + "&range=" + range
+    const response =
+      await axios.get(address)
+    console.log(response.data)
+  };
+
 
   return (
     <nav className="m-0 p-0 navbar navbar-dark fixed-top custombg-dark flex-md-nowrap p-0 shadow">
@@ -20,8 +26,9 @@ const NavBar = () => {
 
       <input className="form-control custominput" type="text" placeholder="Search" aria-label="Search" ></input>
       
-      <Link to="/map"><button className="btn custombtn navitem custombg-orange text-light">Map</button></Link>
-
+      <Link to="/map">
+        <button className="btn custombtn navitem custombg-orange text-light" >Map</button>
+      </Link>
       <div className="customseparator"></div>
 
       <span className="navbar-nav col-1 p-0">
