c49de2588b2e7a61cd86989d8c94047435d8782d
kggu <miikkakortelainen1@gmail.com>
2020-02-27 10:05:22 +0200

    initial popup styling

--- src/components/HotspotPopup.js
+++ src/components/HotspotPopup.js
@@ -1,19 +1,31 @@
 import { Popup } from "react-map-gl";
 import React from "react";
-import Logo from "../logo.png";
+import { Link } from "react-router-dom";
 
-const HotspotPopup = () => {
+import "./popup.css";
 
+const HotspotPopup = props => {
   return (
     <Popup
       tipSize={5}
-      anchor="top"
+      anchor="bottom"
       longitude={25.473}
       latitude={65.013}
-      closeOnClick={false}
+      closeButton={false}
+      offsetTop={-25}
     >
-      <div>
-        <p>test</p>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Place name</h5>
+          <p className="card-text text-style-light">
+            Place description
+          </p>
+          <Link to="">
+            <button className="btn custombtn custombg-orange text-light">
+              Overview
+            </button>
+          </Link>
+        </div>
       </div>
     </Popup>
   );
