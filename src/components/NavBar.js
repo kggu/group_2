e4caf0764a818284319f8a595e105e92771f9f0b
Sami Jouppila <sami.jouppila@gmail.com>
2020-02-27 10:05:11 +0200

    Post request attempt ongoing, currently not working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,36 @@
 // src/components/NavBar.js
 
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "../react-auth0-spa";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 import axios from "axios";
+import postrequest from "./postrequest.json";
 
 const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, getTokenSilently} = useAuth0();
+
+  const [request, changeRequest] = useState(postrequest);
 
   const sendGetRequest = async (longitude, latitude, range) => {
     const address = process.env.REACT_APP_API_ROOT + "/hotspot/search?longitude=" + longitude + "&latitude=" + latitude + "&range=" + range
-    const response =
-      await axios.get(address)
+    const response = await axios.get(address)
     console.log(response.data)
+  };
+
+  const sendPostRequest = async (request) => {
+    const token = await getTokenSilently();
+    
+    const address = process.env.REACT_APP_API_ROOT + "/hotspot"
+
+    let axiosConfig = {
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+      }
+    };
+    const response = await axios.post(address, request, axiosConfig)
+    console.log(token)
   };
 
 
@@ -26,9 +43,9 @@ const NavBar = () => {
 
       <input className="form-control custominput" type="text" placeholder="Search" aria-label="Search" ></input>
       
-      <Link to="/map">
-        <button className="btn custombtn navitem custombg-orange text-light" >Map</button>
-      </Link>
+      {/*<Link to="/map">*/}
+        <button className="btn custombtn navitem custombg-orange text-light" onClick={() => sendPostRequest(request)}>Map</button>
+      {/*</Link>*/}
       <div className="customseparator"></div>
 
       <span className="navbar-nav col-1 p-0">
@@ -37,7 +54,6 @@ const NavBar = () => {
         )}
         {isAuthenticated && <button className="btn custombtn navitem m-0 custombg-orange text-light" onClick={() => logout()}>Log out</button>}
       </span>
-
     </nav>
   );
 };
