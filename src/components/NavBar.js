d30e2b179d82702cd7497bfe053f40a49dbd1a8a
Sami Jouppila <sami.jouppila@gmail.com>
2020-03-11 10:46:01 +0200

    Removed extraneous comments and bootstrap definitions

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,6 @@ import { geocodeByPlaceId } from 'react-google-places-autocomplete';
 import 'react-google-places-autocomplete/dist/assets/index.css';
 import {Navbar} from 'react-bootstrap'
 import {Nav} from 'react-bootstrap'
-import {Form} from 'react-bootstrap'
 import {Button} from 'react-bootstrap'
 
 
@@ -32,34 +31,6 @@ const NavBar = () => {
     })
     .catch(error => console.error(error));
   };
-  {/*}
-    <nav className="m-0 p-0 navbar navbar-dark fixed-top custombg-dark flex-md-nowrap shadow">
-      <Link to="/">
-        <button className="btn custombtn navitem custombg-orange text-light">Home</button>
-      </Link>
-      <div className="customseparator"></div>
-
-      <div className="m-0 p-0">
-        <GooglePlacesAutocomplete 
-        onSelect={({ place_id }) => (
-          findLongLat(place_id)
-        )}/>
-      </div>
-      
-      <Link to="/map">
-        <button className="btn custombtn navitem custombg-orange text-light" >Map</button>
-      </Link>
-      <div className="customseparator"></div>
-
-      <span className="navbar-nav col-1 p-0">
-        {!isAuthenticated && (
-          <button className="btn custombtn navitem m-0 custombg-orange text-light" onClick={() => loginWithRedirect({})}>Log in</button>
-        )}
-        {isAuthenticated && <button className="btn custombtn navitem m-0 custombg-orange text-light" onClick={() => logout()}>Log out</button>}
-      </span>
-
-    </nav>
-    */}
 
 
   return (
@@ -75,15 +46,11 @@ const NavBar = () => {
           findLongLat(place_id)
         )}/>
     {!isAuthenticated && (
-        <Button size="md" variant="customorange" onClick={() => loginWithRedirect({})}>Log in</Button>
+        <Button variant="customorange" onClick={() => loginWithRedirect({})}>Log in</Button>
     )}
     {isAuthenticated &&  (
-        <Button width="100px" variant="customorange" onClick={() => logout()}>Log out</Button>
+        <Button variant="customorange" onClick={() => logout()}>Log out</Button>
     )}
-    {/*<Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>*/}
     
   </Navbar.Collapse>
   </Navbar>
