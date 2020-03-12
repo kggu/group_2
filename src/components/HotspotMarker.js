70e5d81f1b9916215ca95c7e30315a7ee9b4892e
kggu <miikkakortelainen1@gmail.com>
2020-03-12 11:00:48 +0200

    Update HotspotMarker.js

--- src/components/HotspotMarker.js
+++ src/components/HotspotMarker.js
@@ -1,33 +1,26 @@
-import React, { useState } from "react";
 import Logo from "../logo.png";
+import React from 'react'
 import HotspotPopup from "./HotspotPopup";
 
-
-
-
-const HotspotMarker = (props) => {
+const HotspotMarker = props => {
   const size = 30;
 
   return (
     <div>
-      <img src = {Logo}
-      width = {size}
-      height = {size}
-      style={{
+      <img
+        src={Logo}
+        width={size}
+        height={size}
+        style={{
           cursor: "pointer",
           fill: "#d00",
           stroke: "none",
           transform: `translate(${-size / 2}px,${-size}px)`
-          }}
-          onClick={() => props.handler(props.slug)}
-
-          ></img>
+        }}
+        onClick={() => props.handler(props.slug)}
+      ></img>
     </div>
   );
 };
-
-
-
-
 
 export default HotspotMarker;
