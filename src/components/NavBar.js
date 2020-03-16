58c4ca2de0e7d8f297d122837957ae531df2c013
Sami Jouppila <sami.jouppila@gmail.com>
2020-03-16 10:11:49 +0200

    BackendApi CreateNewHotSpot function ready for test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,6 @@ import { useAuth0 } from "../react-auth0-spa";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 import axios from "axios";
-<<<<<<< HEAD
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import { geocodeByPlaceId } from 'react-google-places-autocomplete';
 import 'react-google-places-autocomplete/dist/assets/index.css';
@@ -14,14 +13,10 @@ import {Nav} from 'react-bootstrap'
 import {Button} from 'react-bootstrap'
 import history from "../utils/history";
 
-=======
-import postrequest from "./postrequest.json";
->>>>>>> 7ac899049c7aa380d9c18f04089e77bfb3462868
+
 
 const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout, getTokenSilently} = useAuth0();
-
-  const [request, changeRequest] = useState(postrequest);
+  const { isAuthenticated, loginWithRedirect, logout} = useAuth0();
 
   const sendGetRequest = async (longitude, latitude, range) => {
     const address = process.env.REACT_APP_API_ROOT + "/hotspot/search?longitude=" + longitude + "&latitude=" + latitude + "&range=" + range
@@ -29,7 +24,6 @@ const NavBar = () => {
     console.log(response.data)
   };
 
-<<<<<<< HEAD
   const findLongLat = async (place_id) => {
     geocodeByPlaceId(place_id)
     .then(results =>  {
@@ -45,9 +39,7 @@ const NavBar = () => {
     <Navbar fill variant="customdark" expand="lg">
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Button variant="customorange" className="mr-sm-2" href="/">Home</Button>
-    </Nav>
+    <Button variant="customorange" className="mr-sm-2" href="/">Home</Button>
     <GooglePlacesAutocomplete 
       onSelect={({ place_id }) => (
         findLongLat(place_id)
@@ -59,47 +51,8 @@ const NavBar = () => {
     {isAuthenticated &&  (
         <Button variant="customorange" onClick={() => logout()}>Log out</Button>
     )}
-    
   </Navbar.Collapse>
   </Navbar>
-=======
-  const sendPostRequest = async (request) => {
-    const token = await getTokenSilently();
-    
-    const address = process.env.REACT_APP_API_ROOT + "/hotspot"
-
-    let axiosConfig = {
-      headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token
-      }
-    };
-    const response = await axios.post(address, request, axiosConfig);
-  };
-
-
-  return (
-    <nav className="m-0 p-0 navbar navbar-dark fixed-top custombg-dark flex-md-nowrap p-0 shadow">
-      <Link to="/">
-        <button className="btn custombtn navitem custombg-orange text-light">Home</button>
-      </Link>
-      <div className="customseparator"></div>
-
-      <input className="form-control custominput" type="text" placeholder="Search" aria-label="Search" ></input>
-      
-      {/*<Link to="/map">*/}
-        <button className="btn custombtn navitem custombg-orange text-light" onClick={() => sendPostRequest(request)}>Map</button>
-      {/*</Link>*/}
-      <div className="customseparator"></div>
-
-      <span className="navbar-nav col-1 p-0">
-        {!isAuthenticated && (
-          <button className="btn custombtn navitem m-0 custombg-orange text-light" onClick={() => loginWithRedirect({})}>Log in</button>
-        )}
-        {isAuthenticated && <button className="btn custombtn navitem m-0 custombg-orange text-light" onClick={() => logout()}>Log out</button>}
-      </span>
-    </nav>
->>>>>>> 7ac899049c7aa380d9c18f04089e77bfb3462868
   );
 };
 
--- src/utils/backendAPI.js
+++ src/utils/backendAPI.js
@@ -8,8 +8,9 @@ export const BackendAPIContext = React.createContext();
 export const useBackendAPI = () => useContext(BackendAPIContext);
 
 export const BackendAPIProvider = ({children}) => {
+  const { getTokenSilently } = useAuth0();
   
-  const [hotSpots, setHotSpots] = useState()
+  const [hotSpots, setHotSpots] = useState();
 
   const [request, changeRequest] = useState(postrequest);
 
@@ -24,10 +25,24 @@ export const BackendAPIProvider = ({children}) => {
       viewport.latitude +
       "&range=" +        
       500000000;
-      const response = await axios.get(address).then( response => {
-          setHotSpots(response.data)
-      });
-  }
+    const response = await axios.get(address).then( response => {
+      setHotSpots(response.data)
+    });
+  };
+
+  const createNewHotSpot = async (request) => {
+    const token = await getTokenSilently();
+
+    const address = process.env.REACT_APP_API_ROOT + "/hotspot"
+
+    let axiosConfig = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    }
+    const response = await axios.post(address, request, axiosConfig);
+  };
 
   /*const sendPostRequest = async (request) => {
     const token = await getTokenSilently();
@@ -47,7 +62,8 @@ export const BackendAPIProvider = ({children}) => {
     <BackendAPIContext.Provider
       value={{
           updateHotSpots,
-          hotSpots
+          hotSpots,
+          createNewHotSpot
       }}
     >
     {children}
