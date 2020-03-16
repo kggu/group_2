70e6d3c172b46e7d5b83c6d35b4f273270364f16
t8hosa01 <46774951+t8hosa01@users.noreply.github.com>
2020-03-16 09:43:13 +0200

    fghj

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useBackendAPI } from "../utils/backendAPI";
-import MapGL, { Marker } from "react-map-gl";
+import React, { useState } from "react";
+import MapGL, { Marker,Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
@@ -8,36 +7,17 @@ import { Button } from "react-bootstrap";
 
 import HotspotMarker from "./HotspotMarker";
 import HotspotPopup from './HotspotPopup';
+import NewHotspotPopup from './NewHotspotPopup';
 import SideBar from "./SideBar";
-import NewHotspotPopup from "./NewHotspotPopup";
 
-const Map = props => {
+const Map = () => {
   const [viewport, setViewPort] = useState({
     width: "100%",
-
-    height: window.innerHeight,
+    height: 700,
     latitude: 65.013,
     longitude: 25.47,
     zoom: 16
   });
-  
-  const { updateHotSpots, hotSpots } = useBackendAPI();
-
-  //createNewHotspot(request);
-
-  useEffect(() => {
-    updateViewportFromCoordinates(props.match.params.lat, props.match.params.lng);
-  }, [props.match.params.lat, props.match.params.lng]);
-
-  const updateViewportFromCoordinates = (lat, lng) => {
-    lat = parseFloat(lat);
-    lng = parseFloat(lng);
-    if (!(isNaN(lat) || isNaN(lng))) {
-      if (lat < 90 && lat > -90) {
-        setViewPort({...viewport, latitude: lat, longitude: lng})
-      }
-    }
-  };
 
   const _onViewportChange = viewport =>
     setViewPort({ ...viewport});
@@ -47,6 +27,10 @@ const Map = props => {
   const [clickLocation, setClickLocation] = React.useState([]);
 
   const [modalShow, setModalShow] = React.useState(false);
+   
+  const _onClickMarker = () => {
+    setRender(true);
+  };
 
   const _onClickCreate = () => {
     setRender(true);
@@ -58,71 +42,13 @@ const Map = props => {
     setClickLocation(clickLocation => [...clickLocation, { longitude, latitude }]);
   };
 
-  const [data, setData] = useState();
-  const [markers, setMarkers] = useState();
-  const [selectedMarker, setSelectedMarker] = useState();
-
-  const _onClickMarker = clickedMarker => {
-    if (clickedMarker === selectedMarker) {
-      //if user clicks same marker again
-      setSelectedMarker("");
-      setRender(false);
-      return;
-    }
-
-    setSelectedMarker(clickedMarker);
-
-    if (!render) {
-      setRender(true);
-    }
-  };
-
-  const loadMarkers = () => {
-    console.log(data)
-    setMarkers(
-      data.map(spot => {
-        return (
-          <Marker
-            key={spot.slug}
-            latitude={parseFloat(spot.location.latitude)}
-            longitude={parseFloat(spot.location.longitude)}
-          >
-            {render && spot.slug == selectedMarker && (
-              <HotspotPopup
-                longitude={parseFloat(spot.location.longitude)}
-                latitude={parseFloat(spot.location.latitude)}
-                name={spot.name}
-                description={spot.description}
-                slug={spot.slug}
-              />
-            )}
-            <HotspotMarker handler={_onClickMarker} slug={spot.slug} />
-          </Marker>
-        );
-      })
-    );
-  };
-
-  useEffect(() => {
-    updateHotSpots(viewport)
-  }, []);
-
-  useEffect(() => {
-    setData(hotSpots)
-  }, [hotSpots]);
-
-  useEffect(() => {
-    if (data) {
-      loadMarkers();
-    }
-  }, [selectedMarker, render, data]);
 
   return (
-    <div className="container-fluid px-0">
+    <div className="container-fluid testclass">
       <div className="col-md-2 d-none d-md-block bg-light sidebar-4">
-        <SideBar />
+      <SideBar />
       </div>
-      <div className="col-md-9 ml-sm-auto col-lg-10 px-0">
+      <div className="col-md-9 ml-sm-auto col-lg-10 px-4">
         <MapGL
           {...viewport}
           mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
@@ -151,10 +77,9 @@ const Map = props => {
            
             </HotspotMarker>
           </Marker>
-          {markers}
         </MapGL>
       </div>
-    </div>
+      </div>
   );
 };
 
