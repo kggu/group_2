0bc98f50ece89a211427cedbd58b0c54bc18248b
kggu <miikkakortelainen1@gmail.com>
2020-04-06 12:27:22 +0300

    Update HotspotInfo layout

--- src/components/OverviewPage/HotspotInfo.js
+++ src/components/OverviewPage/HotspotInfo.js
@@ -1,25 +1,40 @@
 import React from "react";
-import { ListGroup } from "react-bootstrap";
+import { Image, Button } from "react-bootstrap";
 
 const HotspotInfo = props => {
   return (
     <div>
-            <div className="hotspot-header">
-        <div>{props.hotspotInfo.name}</div>
-
+      <div className="hotspot-header">
+        <div>
+          <a>{props.hotspotInfo.name}</a>
+        </div>
       </div>
-    <div className="hotspot-info">
-
-      <div className="hotspot-address">
-        {" "}
-        {props.hotspotInfo.address.address},
-        <br></br>
-        {props.hotspotInfo.address.postalCode} {props.hotspotInfo.address.city},{" "}
-        {props.hotspotInfo.address.country}
-
+      <div className="hotspot-info">
+        <div className="hotspot-address">
+          {" "}
+          {props.hotspotInfo.address.address}
+          {","} {props.hotspotInfo.address.postalCode}{" "}
+          {props.hotspotInfo.address.city}
+          <br></br>
+          {props.hotspotInfo.address.country} <br></br>
+          <br></br>
+          <div className="hotspot-creator-info">
+            {/*<Image src={props.hotspotInfo.creator.picture}*/} Created by{" "}
+            {props.hotspotInfo.creator.nickname}
+          </div>
+        </div>
+        <div className="hotspot-rating">
+          RATING GOES HERE
+          <div className="voting-controls">
+            <Button className="vote-button" onClick="" variant="">
+              +
+            </Button>
+            <Button className="vote-button" onclick="" variant="">
+              -
+            </Button>
+          </div>
+        </div>
       </div>
-      <div>Created by {props.hotspotInfo.creator.name}</div>
-    </div>
     </div>
   );
 };
