4e484db7ed62a17e512be2e87d0ddad5fe94fe2c
kggu <miikkakortelainen1@gmail.com>
2020-04-09 12:29:45 +0300

    Fetch openingHours from data, Fix weekDay order

--- src/components/OverviewPage/OpeningHoursTable.js
+++ src/components/OverviewPage/OpeningHoursTable.js
@@ -1,38 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 
-const OpeningHoursTable = props => {
+const OpeningHoursTable = (props) => {
+  const enumDays = {
+    MONDAY: 1,
+    TUESDAY: 2,
+    WEDNESDAY: 3,
+    THURSDAY: 4,
+    FRIDAY: 5,
+    SATURDAY: 6,
+    SUNDAY: 7,
+  };
+
+  props.openingHours.sort((a, b) => enumDays[a.weekDay] > enumDays[b.weekDay]);
+
   return (
     <Table striped size="sm" bordered variant="light">
       <tbody>
-        <tr>
-          <td>Monday</td>
-          <td>00.00 - 00.00</td>
-        </tr>
-        <tr>
-          <td>Tuesday</td>
-          <td>00.00 - 00.00</td>
-        </tr>
-        <tr>
-          <td>Wednesday</td>
-          <td>00.00 - 00.00</td>
-        </tr>
-        <tr>
-          <td>Thursday</td>
-          <td>00.00 - 00.00</td>
-        </tr>
-        <tr>
-          <td>Friday</td>
-          <td>00.00 - 00.00</td>
-        </tr>
-        <tr>
-          <td>Saturday</td>
-          <td>00.00 - 00.00</td>
-        </tr>
-        <tr>
-          <td>Sunday</td>
-          <td>00.00 - 00.00</td>
-        </tr>
+        {props.openingHours.map(function (day, i) {
+          return (
+            <tr>
+              <td>{day.weekDay}</td>
+              <td>
+                <small>{day.openingTime} - {day.closingTime}</small>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </Table>
   );
