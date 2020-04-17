1430c8f7fcb8c7629dc80d075add79390fc7ca12
kggu <miikkakortelainen1@gmail.com>
2020-04-17 13:30:26 +0300

    Update confirmation modal

--- src/components/OverviewPage/Hotspot/EditHotspot.js
+++ src/components/OverviewPage/Hotspot/EditHotspot.js
@@ -6,69 +6,174 @@ import {
   Modal,
   Form,
   Col,
+  Row,
   Alert,
 } from "react-bootstrap";
 
 const ReviewHotspotChanges = (props) => {
-    console.log(props);
+  console.log(props);
 
-    return (
-      <Modal
-        {...props}
-        size="md"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Review changes
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Row>
-                <Form.Control plaintext value={props.hotspotData.address.city} />
-                </Form.Row>
-              </Form.Group>
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Review changes
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control
+                  plaintext
+                  value={props.hotspotData.name}
+                />
+              </Form.Row>
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control plaintext value={">"} />
+              </Form.Row>
+            </Form.Group>
 
-              <Form.Group as={Col} controlId="formGridCountry">
-                <Form.Label>Country</Form.Label>
-                <Form.Control value={props.hotspotData.address.country} />
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Control value={props.hotspotData.address.city} />
-              </Form.Group>
+            <Form.Group as={Col} controlId="formGridCountry">
+              <Form.Control plaintext value={"placeholder"} />
+            </Form.Group>
+          </Form.Row>
 
-              <Form.Group as={Col} controlId="formGridCountry">
-                <Form.Label>Country</Form.Label>
-                <Form.Control value={props.hotspotData.address.country} />
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Control value={props.hotspotData.address.city} />
-              </Form.Group>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control
+                  plaintext
+                  value={props.hotspotData.description}
+                />
+              </Form.Row>
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control plaintext value={">"} />
+              </Form.Row>
+            </Form.Group>
 
-              <Form.Group as={Col} controlId="formGridCountry">
-                <Form.Label>Country</Form.Label>
-                <Form.Control value={props.hotspotData.address.country} />
-              </Form.Group>
-            </Form.Row>
+            <Form.Group as={Col} controlId="formGridCountry">
+              <Form.Control plaintext value={"placeholder"} />
+            </Form.Group>
+          </Form.Row>
 
-            <Button variant="primary" type="submit">
-              Request changes
-            </Button>
-          </Form>
-        </Modal.Body>
-      </Modal>
-    );
-  };
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control
+                  plaintext
+                  value={props.hotspotData.category}
+                />
+              </Form.Row>
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control plaintext value={">"} />
+              </Form.Row>
+            </Form.Group>
 
-  export default ReviewHotspotChanges;
+            <Form.Group as={Col} controlId="formGridCountry">
+              <Form.Control plaintext value={"placeholder"} />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control
+                  plaintext
+                  value={props.hotspotData.address.city}
+                />
+              </Form.Row>
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control plaintext value={">"} />
+              </Form.Row>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridCountry">
+              <Form.Control plaintext value={"placeholder"} />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control
+                  plaintext
+                  value={props.hotspotData.address.address}
+                />
+              </Form.Row>
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control plaintext value={">"} />
+              </Form.Row>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridCountry">
+              <Form.Control plaintext value={"placeholder"} />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control
+                  plaintext
+                  value={props.hotspotData.address.postalCode}
+                />
+              </Form.Row>
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control plaintext value={">"} />
+              </Form.Row>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridCountry">
+              <Form.Control plaintext value={"placeholder"} />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control
+                  plaintext
+                  value={props.hotspotData.address.country}
+                />
+              </Form.Row>
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Row>
+                <Form.Control plaintext value={">"} />
+              </Form.Row>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridCountry">
+              <Form.Control plaintext value={"placeholder"} />
+            </Form.Group>
+          </Form.Row>
+
+          <Button variant="primary" type="submit">
+            Request changes
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default ReviewHotspotChanges;
