02f2798e62e11f489b6151cad3ac9b385f4f627d
Sami Jouppila <sami.jouppila@gmail.com>
2020-04-20 15:23:02 +0300

    Reusable component ReviewHotSpotChangesForm added

--- src/components/AdminPage/AdminActions.js
+++ src/components/AdminPage/AdminActions.js
@@ -4,8 +4,13 @@ import Tab from "react-bootstrap/Tab"
 import "./AdminActions.css";
 import AdminPageUserList from "./AdminPageUserList";
 
+import Button from "react-bootstrap/Button"
+import ReviewHotSpotChangesForm from "../Reusable/ReviewHotSpotChangesForm";
+
 const AdminActions = () => {
     const [key, setKey] = useState('View suggestions')
+
+    const [show, setShow] = useState(false);
 
     return <Tabs
             id="Admin actions"
@@ -13,7 +18,9 @@ const AdminActions = () => {
             onSelect={(k) => setKey(k)}
             className="custom-tabs">
                 <Tab eventKey="View suggestions" title="View suggestions" tabClassName="bg-darkblue">
-                    <p>In progress.</p>
+                    <Button onClick={() => {setShow(true)}}></Button>
+                    {show && <ReviewHotSpotChangesForm slug={"kaupunginkirjasto"} show={show} onHide={() => {setShow(false)}}></ReviewHotSpotChangesForm>}
+                    
                 </Tab>
                 <Tab eventKey="Manage users" title="Manage Users" tabClassName="bg-darkblue">
                     <AdminPageUserList></AdminPageUserList>
---
+++ src/components/Reusable/ReviewHotSpotChangesForm.js
@@ -0,0 +1,231 @@
+import React, {useState, useEffect} from "react";
+import Modal from "react-bootstrap/Modal";
+import Spinner from "react-bootstrap/Spinner"
+import Form from "react-bootstrap/Form"
+import Col from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import { useBackendAPI } from "../../utils/backendAPI";
+
+const ReviewHotSpotChangesForm = props => {
+    const [loading, setLoading] = useState(false);
+    const [ oldData, setOldData ] = useState();
+
+    const { selectedHotspot, getHotspotWithSlug } = useBackendAPI();
+    
+
+    useEffect(() => {
+        setOldData();
+        setLoading(true);
+        getHotspotWithSlug(props.slug);
+    },[])
+
+    useEffect(() => {
+        if (selectedHotspot) {
+            setLoading(false);
+            setOldData(selectedHotspot)
+        }
+    },[selectedHotspot])
+
+    return (
+    <Modal
+    {...props}
+    size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+    >
+        <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+            Review changes
+            </Modal.Title>
+        </Modal.Header>
+        {loading && ( <Spinner animation="border" role="status"> </Spinner> )}
+
+        { oldData && (
+            <Modal.Body>
+            <Form>
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={"Name:"} />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control
+                      plaintext
+                      value={oldData.name}
+                    />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={">"} />
+                  </Form.Row>
+                </Form.Group>
+    
+                <Form.Group as={Col}>
+                  <Form.Control plaintext value={"placeholder"} />
+                </Form.Group>
+              </Form.Row>
+    
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={"Description:"} />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control
+                      plaintext
+                      value={oldData.description}
+                    />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col} >
+                  <Form.Row>
+                    <Form.Control plaintext value={">"} />
+                  </Form.Row>
+                </Form.Group>
+    
+                <Form.Group as={Col}>
+                  <Form.Control plaintext value={"placeholder"} />
+                </Form.Group>
+              </Form.Row>
+    
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={"Category:"} />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control
+                      plaintext
+                      value={oldData.category}
+                    />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={">"} />
+                  </Form.Row>
+                </Form.Group>
+    
+                <Form.Group as={Col}>
+                  <Form.Control plaintext value={"placeholder"} />
+                </Form.Group>
+              </Form.Row>
+    
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={"City"} />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control
+                      plaintext
+                      value={oldData.address.city}
+                    />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={">"} />
+                  </Form.Row>
+                </Form.Group>
+    
+                <Form.Group as={Col} >
+                  <Form.Control plaintext value={"placeholder"} />
+                </Form.Group>
+              </Form.Row>
+    
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={"Address:"} />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control
+                      plaintext
+                      value={oldData.address.address}
+                    />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={">"} />
+                  </Form.Row>
+                </Form.Group>
+    
+                <Form.Group as={Col}>
+                  <Form.Control plaintext value={"placeholder"} />
+                </Form.Group>
+              </Form.Row>
+    
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={"Postal code:"} />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control
+                      plaintext
+                      value={oldData.address.postalCode}
+                    />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={">"} />
+                  </Form.Row>
+                </Form.Group>
+    
+                <Form.Group as={Col}>
+                  <Form.Control plaintext value={"placeholder"} />
+                </Form.Group>
+              </Form.Row>
+    
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={"Country:"} />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control
+                      plaintext
+                      value={oldData.address.country}
+                    />
+                  </Form.Row>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Row>
+                    <Form.Control plaintext value={">"} />
+                  </Form.Row>
+                </Form.Group>
+    
+                <Form.Group as={Col}>
+                  <Form.Control plaintext value={"placeholder"} />
+                </Form.Group>
+              </Form.Row>
+    
+              <Button variant="primary" type="submit">
+                Request changes
+              </Button>
+            </Form>
+          </Modal.Body>
+        )}
+    </Modal>
+    )
+};
+
+export default ReviewHotSpotChangesForm;
