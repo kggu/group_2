cbb866109e67b956eb615e1aed4fa2bc318e1aee
kggu <miikkakortelainen1@gmail.com>
2020-04-21 19:40:34 +0300

    Add star rating actions

--- src/components/OverviewPage/Hotspot/HotspotRating.js
+++ src/components/OverviewPage/Hotspot/HotspotRating.js
@@ -1,45 +1,67 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { useBackendAPI } from "../../../utils/backendAPI";
+import { StarRating } from "./StarRating";
 
 const HotspotRating = (props) => {
-  const { rateHotspot, rateHotSpotResolved, getHotspotWithSlug, selectedHotspot } = useBackendAPI();
-  const [userRating, setUserRating] = useState();
+  const {
+    rateHotspot,
+    rateHotSpotResolved,
+    getHotspotWithSlug,
+    selectedHotspot,
+  } = useBackendAPI();
   const [ratingSent, setRatingSent] = useState(false);
+
+  const [ratingCount, setRatingCount] = useState(0);
+  const [userRating, setUserRating] = useState();
+  const [hasRatings, setHasRatings] = useState(false);
+
+  useEffect(() => {
+    if (props.ratings) setRatingCount(props.ratings.length);
+    if (props.ratings.length > 0) {
+      setHasRatings(true);
+    }
+  }, []);
 
   useEffect(() => {
     setRatingSent(false);
-  },[selectedHotspot]);
+  }, [selectedHotspot]);
 
-  const _handleChange = (e) => {
-    setUserRating(e.target.value);
+  const _handleChange = (starsSelected) => {
+    setUserRating(starsSelected);
   };
 
   const _rateHotspot = () => {
     const rating = {
       rating: userRating,
     };
-    setRatingSent(true)
+    setRatingSent(true);
     rateHotspot(rating, props.slug);
   };
 
   useEffect(() => {
-    if (ratingSent && rateHotSpotResolved && rateHotSpotResolved.status == 200) {
+    if (
+      ratingSent &&
+      rateHotSpotResolved &&
+      rateHotSpotResolved.status == 200
+    ) {
       getHotspotWithSlug(props.slug);
     }
-  },[rateHotSpotResolved]);
+  }, [rateHotSpotResolved]);
 
   return (
     <div className="hotspot-rating">
-      <a>
-        Average rating: {props.ratingAverage}
-        <br></br>
-      </a>
-      <a>Rated by {props.ratings.length} students</a>
-      <div className="rating-test">
-        <input onChange={_handleChange} className="test" type="text"></input>
+      {!hasRatings && <a>No ratings yet. Be the first one!</a>}
+      {hasRatings && <a>Average rating: {props.ratingAverage}</a>}
+
+      <br />
+      {hasRatings && <a>Rated by {ratingCount} students</a>}
+
+      <div className="rating-actions">
+        <StarRating onChange={_handleChange} totalStars={5} />
+        {/*  <input onChange={_handleChange} className="test" type="text"></input>*/}
         <Button onClick={_rateHotspot} variant="" className="rate-button-test">
-          test (0-5)
+          rate
         </Button>
       </div>
       <div className="rated-by">
